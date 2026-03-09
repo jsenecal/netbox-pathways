84a53135d39353370fe70dd93fe0eaed057c5796
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2026-03-09 17:36:50 +0000

    feat(search): add model-specific fields to search indexes
    Index structure_type, material, aerial_type, armor_type, size, color,
    position, configuration, encasement_type, bank_position, and
    messenger_size for richer global search results.

--- netbox_pathways/search.py
+++ netbox_pathways/search.py
@@ -8,6 +8,7 @@ class StructureIndex(SearchIndex):
     model = models.Structure
     fields = (
         ('name', 100),
+        ('structure_type', 200),
         ('access_notes', 500),
         ('comments', 5000),
     )
@@ -19,6 +20,8 @@ class ConduitIndex(SearchIndex):
     model = models.Conduit
     fields = (
         ('name', 100),
+        ('material', 200),
+        ('bank_position', 300),
         ('comments', 5000),
     )
     display_attrs = ('material', 'start_structure', 'end_structure')
@@ -29,6 +32,8 @@ class AerialSpanIndex(SearchIndex):
     model = models.AerialSpan
     fields = (
         ('name', 100),
+        ('aerial_type', 200),
+        ('messenger_size', 300),
         ('comments', 5000),
     )
     display_attrs = ('aerial_type', 'start_structure', 'end_structure')
@@ -39,6 +44,7 @@ class DirectBuriedIndex(SearchIndex):
     model = models.DirectBuried
     fields = (
         ('name', 100),
+        ('armor_type', 200),
         ('comments', 5000),
     )
     display_attrs = ('start_structure', 'end_structure')
@@ -49,6 +55,9 @@ class InnerductIndex(SearchIndex):
     model = models.Innerduct
     fields = (
         ('name', 100),
+        ('size', 200),
+        ('color', 300),
+        ('position', 300),
         ('comments', 5000),
     )
     display_attrs = ('parent_conduit', 'size', 'color')
@@ -59,6 +68,8 @@ class ConduitBankIndex(SearchIndex):
     model = models.ConduitBank
     fields = (
         ('name', 100),
+        ('configuration', 200),
+        ('encasement_type', 300),
         ('comments', 5000),
     )
     display_attrs = ('structure', 'configuration', 'total_conduits')
@@ -71,7 +82,7 @@ class ConduitJunctionIndex(SearchIndex):
         ('name', 100),
         ('comments', 5000),
     )
-    display_attrs = ('trunk_conduit', 'branch_conduit')
+    display_attrs = ('trunk_conduit', 'branch_conduit', 'towards_structure')
 
 
 @register_search
